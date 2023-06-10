7efccaeccd088a7cfc247ca8a9933e894154ccbe
menta1breakdown <crutiychuvak@gmail.com>
2023-06-10 17:56:06 +0300

    no message

---
+++ lab7.py
@@ -0,0 +1,34 @@
+from telegram.ext import Updater, CommandHandler, MessageHandler,Filters
+from elasticsearch import Elasticsearch
+
+es = Elasticsearch('localhost:9200')
+def start(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Lab7 EFK")
+def log_message(update, context):
+    message = update.message.text
+    chat_id = update.effective_chat.id
+
+    doc = {
+        'message': message,
+        'chat_id': chat_id
+    }
+    es.index(index='telegram_logs', doc_type='_doc', body=doc)
+
+def error(update, context):
+    print(f"Помилка: {context.error}")
+def main():
+    token = '6191130254:AAH5yCH0XWUYBuSNw-XCaQGX3RDJ4p1v920'
+    updater = Updater(token=token, use_context=True)
+    dispatcher = updater.dispatcher
+
+    dispatcher.add_handler(CommandHandler("start", start))
+    dispatcher.add_handler(MessageHandler(Filters.text,log_message))
+
+    dispatcher.add_error_handler(error)
+
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
+
